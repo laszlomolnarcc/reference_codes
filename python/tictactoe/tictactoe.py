acc97a0c161dc4b7959cc01309e5d9b929a1b85d
Laszlo Molnar <laszlo.molnar@codecool.com>
2016-10-07 17:44:44 +0200

    Working TicTacToe, draw match is not yet handled!

--- python/tictactoe/tictactoe.py
+++ python/tictactoe/tictactoe.py
@@ -3,6 +3,10 @@
 # Laszlo Molnar, 2016, Codecool
 import os
 
+#global variables
+table = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
+currentPlayer = "X"
+
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -20,14 +24,137 @@ def processMenuDecision():
     while (userInput != "1" and userInput != "2"):
         userInput = input("Choose from the numbers (write 1 or 2 and press ENTER): ")
     if "1" == userInput:
-        startTwoPlayerGame()
+        playTwoPlayerGame()
+        return 1
     if "2" == userInput:
         print("Bye!")
+    return 2
 
-def startTwoPlayerGame():
-    print("Start the game!!")
+def playTwoPlayerGame():
+    initGame()
+    global currentPlayer
+    currentPlayer = decideWhomStarts()
+    doGameLoop()
+    print("'" + currentPlayer + "' won the game!")
+    input("Press ENTER to go back to the Main Menu.")
 
-clearScreen()
-showWelcomeInfo()
-showMenu()
-processMenuDecision()
+def initGame():
+    table = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
+
+def decideWhomStarts():
+    clearScreen()
+    whomStarts = ""
+    while whomStarts != "X" and whomStarts != "O":
+        whomStarts = input("'X'or 'O' should start the game (X/O)? ")
+    return whomStarts
+
+def doGameLoop():
+    drawTable()
+    finished = False
+    while finished == False:
+        coords = getTableCoordsFromPlayer()
+        while False == coordsAreValid(coords):
+            print("The place you chosen is not empty!")
+            coords = getTableCoordsFromPlayer()
+        global currentPlayer
+        table[coords[0]][coords[1]] = currentPlayer
+        if True == gameFinished(coords):
+            finished = True
+        else:
+            changeCurrentPlayer()
+        drawTable()
+
+def drawTable():
+    clearScreen()
+    print("    A   B   C")
+    print("  -------------")
+    print("1 |", table[0][0], "|", table[1][0], "|", table[2][0], "|")
+    print("  -------------")
+    print("2 |", table[0][1], "|", table[1][1], "|", table[2][1], "|")
+    print("  -------------")
+    print("3 |", table[0][2], "|", table[1][2], "|", table[2][2], "|")
+    print("  -------------")
+
+def getTableCoordsFromPlayer():
+    coords = (-1, -1)
+    while coords[0] == -1 or coords[1] == -1:
+        global currentPlayer
+        nextMove = input("'" + currentPlayer + "', what is your next move(for ex. A 2)? ")
+        if len(nextMove) > 2:
+            coords = parsePlayerInput(nextMove)
+    return coords
+
+def parsePlayerInput(pInput):
+    col = -1
+    row = -1
+    if pInput[0] == 'A':
+        col = 0
+    if pInput[0] == 'B':
+        col = 1
+    if pInput[0] == 'C':
+        col = 2
+    if pInput[2] == '1':
+        row = 0
+    if pInput[2] == '2':
+        row = 1
+    if pInput[2] == '3':
+        row = 2
+    return col, row
+
+def coordsAreValid(coords):
+    if table[coords[0]][coords[1]] == ".":
+        return True
+    return False
+
+def changeCurrentPlayer():
+    global currentPlayer
+    if currentPlayer == "X": 
+        currentPlayer = "O" 
+    else:
+        currentPlayer = "X"
+
+def gameFinished(lastCoord):
+    # check if the player won
+    global currentPlayer
+
+    # horizontal checking
+    startPoint = checkSameItems(lastCoord, (-1, 0))
+    endPoint = checkSameItems(lastCoord, (1, 0))
+    if endPoint[0] - startPoint[0] >= 2:
+       return True
+    # vertical checking
+    startPoint = checkSameItems(lastCoord, (0, -1))
+    endPoint = checkSameItems(lastCoord, (0, 1))
+    if endPoint[1] - startPoint[1] >= 2:
+       return True
+    # diagonal - left-to-right checking
+    startPoint = checkSameItems(lastCoord, (-1, -1))
+    endPoint = checkSameItems(lastCoord, (1, 1))
+    if endPoint[0] - startPoint[0] >= 2:
+       return True
+    # diagonal - right-to-left checking
+    startPoint = checkSameItems(lastCoord, (1, -1))
+    endPoint = checkSameItems(lastCoord, (-1, 1))
+    if abs(endPoint[0] - startPoint[0]) >= 2:
+       return True
+
+    return False
+
+def checkSameItems(startCoord, incCoord):
+    finalCoord = startCoord
+    nextCoord = (finalCoord[0] + incCoord[0], finalCoord[1] + incCoord[1])
+
+    while nextCoord[0] >= 0 and nextCoord[0] <= 2 and \
+          nextCoord[1] >= 0 and nextCoord[1] <= 2 and \
+          table[nextCoord[0]][nextCoord[1]] == currentPlayer:
+        finalCoord = nextCoord
+        nextCoord = (nextCoord[0] + incCoord[0], nextCoord[1] + incCoord[1])
+    return finalCoord
+
+#main
+menuDecision = 0
+while menuDecision != 2:
+    clearScreen()
+    showWelcomeInfo()
+    showMenu()
+    menuDecision = processMenuDecision()
